9faa7cca0e25b6e94349734affbe3c6a95305528
apolo <apolobenitez65@gmail.com>
2026-06-22 21:46:07 -0300

    modifique la tabla clientes

---
+++ database/migrations/2026_06_23_002740_add_dni_to_clientes_table.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('clientes', function (Blueprint $table) {
+            $table->char('dni', 8)->after('nombre'); // o después de otro campo
+            $table->string('direccion')->after('telefono');
+        });
+    }
+    public function down(): void
+    {
+        Schema::table('clientes', function (Blueprint $table) {
+            $table->dropColumn('dni');
+            $table->dropColumn('direccion');
+        });
+    }
+};
